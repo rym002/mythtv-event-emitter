25cb0db0c06a10ebf772f676ecf1197e4b276871
rym002 <raymond.munian@gmail.com>
2019-08-23 21:41:56 -0400

    Rename endpoint to sender

--- src/events.ts
+++ src/events.ts
@@ -2,12 +2,12 @@ import { EventMapping } from './messages'
 import { EventEmitter } from 'events';
 
 type MythEvent = 'MythEvent';
-type EndpointNotificationTypes = 'pre' | 'post';
+type SenderNotificationTypes = 'pre' | 'post';
 
 export function notifyEvent<T extends keyof EventMapping, P extends EventMapping[T]>(eventType: T, sender: string, eventMessage: P): void {
     mythNotifier.emit('MythEvent', sender, eventType, eventMessage);
 }
-export interface MythEndpointEventEmitter {
+export interface MythSenderEventEmitter {
     emit<T extends keyof EventMapping, P extends EventMapping[T]>(eventType: T, message: P): boolean
     on<T extends keyof EventMapping, P extends EventMapping[T]>(eventType: T, listener: (message: P) => void): this
     once<T extends keyof EventMapping, P extends EventMapping[T]>(eventType: T, listener: (message: P) => void): this
@@ -15,43 +15,43 @@ export interface MythEndpointEventEmitter {
     removeListener<T extends keyof EventMapping, P extends EventMapping[T]>(eventType: T, listener: (message: P) => void): this
     removeAllListeners<T extends keyof EventMapping>(eventType: T): this
 
-    emit<T extends keyof EventMapping, P extends EventMapping[T]>(event: EndpointNotificationTypes, eventType: T, message: P): boolean;
-    on<T extends keyof EventMapping, P extends EventMapping[T]>(event: EndpointNotificationTypes, listener: (eventType: T, message: P) => void): this
-    once<T extends keyof EventMapping, P extends EventMapping[T]>(event: EndpointNotificationTypes, listener: (eventType: T, message: P) => void): this
-    prependListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: EndpointNotificationTypes, listener: (eventType: T, message: P) => void): this
-    removeListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: EndpointNotificationTypes, listener: (eventType: T, message: P) => void): this
-    removeAllListeners(event: EndpointNotificationTypes): this
+    emit<T extends keyof EventMapping, P extends EventMapping[T]>(event: SenderNotificationTypes, eventType: T, message: P): boolean;
+    on<T extends keyof EventMapping, P extends EventMapping[T]>(event: SenderNotificationTypes, listener: (eventType: T, message: P) => void): this
+    once<T extends keyof EventMapping, P extends EventMapping[T]>(event: SenderNotificationTypes, listener: (eventType: T, message: P) => void): this
+    prependListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: SenderNotificationTypes, listener: (eventType: T, message: P) => void): this
+    removeListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: SenderNotificationTypes, listener: (eventType: T, message: P) => void): this
+    removeAllListeners(event: SenderNotificationTypes): this
 }
 
 export interface MythEventEmitter {
-    emit<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, endpoint: string, eventType: T, message: P): boolean;
-    on<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (endpoint: string, eventType: T, message: P) => void): this
-    once<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (endpoint: string, eventType: T, message: P) => void): this
-    removeListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (endpoint: string, eventType: T, message: P) => void): this
-    prependListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (endpoint: string, eventType: T, message: P) => void): this
-    endpoint(endpoint: String): MythEndpointEventEmitter
+    emit<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, sender: string, eventType: T, message: P): boolean;
+    on<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (sender: string, eventType: T, message: P) => void): this
+    once<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (sender: string, eventType: T, message: P) => void): this
+    removeListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (sender: string, eventType: T, message: P) => void): this
+    prependListener<T extends keyof EventMapping, P extends EventMapping[T]>(event: MythEvent, listener: (sender: string, eventType: T, message: P) => void): this
+    sender(sender: String): MythSenderEventEmitter
     removeAllListeners(event: MythEvent): this
 }
 
 class MythEmitter extends EventEmitter implements MythEventEmitter {
-    private endpoints = new Map<string, MythEndpointEventEmitter>();
+    private senders = new Map<string, MythSenderEventEmitter>();
     constructor() {
         super();
-        this.on('MythEvent', this.handleEndpointNotification);
+        this.on('MythEvent', this.handleSenderNotification);
     }
-    public endpoint(endpoint: string): MythEndpointEventEmitter {
-        let ret = this.endpoints.get(endpoint);
+    public sender(sender: string): MythSenderEventEmitter {
+        let ret = this.senders.get(sender);
         if (!ret) {
             ret = new EventEmitter();
-            this.endpoints.set(endpoint, ret);
+            this.senders.set(sender, ret);
         }
         return ret;
     }
-    private handleEndpointNotification<T extends keyof EventMapping, P extends EventMapping[T]>(endpoint: string, eventType: T, message: P) {
-        const endpointEmitter = this.endpoint(endpoint);
-        endpointEmitter.emit('pre', eventType, message);
-        endpointEmitter.emit(eventType, message);
-        endpointEmitter.emit('post', eventType, message);
+    private handleSenderNotification<T extends keyof EventMapping, P extends EventMapping[T]>(sender: string, eventType: T, message: P) {
+        const senderEmitter = this.sender(sender);
+        senderEmitter.emit('pre', eventType, message);
+        senderEmitter.emit(eventType, message);
+        senderEmitter.emit('post', eventType, message);
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { MythEndpointEventEmitter, mythNotifier, MythEventEmitter } from './events';
+export { MythSenderEventEmitter, mythNotifier, MythEventEmitter } from './events';
 export { EmptyEvent, PlayEvent, RecDeletedEvent, RecEvent, RecExpired, RecPending, RemoteHostEvent } from './messages';
 export { router as httpRouter } from './router';
